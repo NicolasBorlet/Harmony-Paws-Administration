0e4ca505e4b4c36185fbda535fbfc9cac45eab46
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-18 22:03:19 +0100

    fix vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import Dashboard from '@/components/dashboard'
 
