aff8e1a47627db78f7af71ee8a1bdf071551c668
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-18 22:04:53 +0100

    fix vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import Dashboard from '@/components/dashboard'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body>
+      <html lang="en" suppressHydrationWarning>
+        <body suppressHydrationWarning>
           <main>
             <Dashboard>
               {children}
